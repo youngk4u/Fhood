74ed33e97b9405fd5649ae20127994126533d8cc
youngk4u <youngk4u@yahoo.com>
2015-08-29 01:21:50 -0700

    Log in View Controller

---
+++ Fhood/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Fhood
+//
+//  Created by YOUNG on 8/29/15.
+//  Copyright © 2015 YOUNG&YOUM. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        self.navigationController?.navigationBarHidden = false
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0/255, green: 255/255, blue: 234/255, alpha: 1.0)
+        self.navigationController?.navigationBar.tintColor = UIColor.blackColor()
+        self.navigationItem.title = "Log in"
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+
+}
