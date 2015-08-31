17a39444acb349c37c6b900c270d7a8af4b4e938
youngk4u <youngk4u@yahoo.com>
2015-08-31 02:24:21 -0700

    Floored More VC created

---
+++ Fhood/FhooderMoreViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FhooderMoreViewController.swift
+//  Fhood
+//
+//  Created by Young-hu Kim on 8/30/15.
+//  Copyright (c) 2015 Fhood LLC. All rights reserved.
+//
+
+import UIKit
+
+class FhooderMoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+    
+
+
+
+}
