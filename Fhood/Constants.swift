c4fe0bc1f26f690273cdac5884c26034d90b2e96
Matias Pequeno <matias.pequeno@gmail.com>
2015-09-06 12:17:48 -0700

    Facebook initialization.

--- Fhood/Constants.swift
+++ Fhood/Constants.swift
@@ -9,8 +9,11 @@
 import Foundation
 
 struct Constants {
+
     struct Vendor {
         static let ParseApplicationID = "Ji19GFajRMo6ruqU64dLdKhjdJLJNeFHN0t7AW1y"
         static let ParseClientKey = "vCP3IBibukwFMF3LdoxyKim7uw1hglzZhWDsQ4BO"
+        static let FacebookAppID = "515338641949786"
+        static let FacebookAppSecret = "0d116ebc6f08aa000a090a43405e607f"
     }
 }
