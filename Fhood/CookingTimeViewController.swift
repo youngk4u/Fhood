35572db4e1fc1e9c62b3205928f509a6b7d36a32
Young-hu Kim <youngk4u@yahoo.com>
2015-10-11 19:02:29 -0700

    Improvement on Cooking time page.

--- Fhood/CookingTimeViewController.swift
+++ Fhood/CookingTimeViewController.swift
@@ -11,41 +11,69 @@ import UIKit
 class CookingTimeViewController: UIViewController {
 
     @IBOutlet weak var cookingSwitch: UISwitch!
+    @IBOutlet weak var openTimeButton: UIButton!
+    @IBOutlet weak var closeTimeButton: UIButton!
+    
+    private var openTimeHour: Int = 0
+    private var openTimeMin: Int = 0
+    private var closeTimeHour: Int = 0
+    private var closeTimeMin: Int = 0
+    private var amPm: Bool = true
+    
+    @IBOutlet weak var scheduleView: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Round corners for scheduleView
+        self.scheduleView.layer.cornerRadius = 7
+        
+        // Initializing the switch
         if variables.isOpen == true {
             self.cookingSwitch.on = true
+            self.scheduleView.alpha = 1
         }
+        else {
+            self.scheduleView.alpha = 0
+        }
+        
         self.cookingSwitch.addTarget(self, action: "switchState:", forControlEvents: UIControlEvents.ValueChanged)
        
     }
     
+    // Switch function for the cooking time on/off
     func switchState (Switch: UISwitch) {
         if Switch.on {
             variables.isOpen = true
             variables.isClosed = false
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.scheduleView.alpha = 1
+                })
         }
         else {
             variables.isClosed = true
             variables.isOpen = false
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.scheduleView.alpha = 0
+            })
         }
     }
 
+    // Closed button for Switch
     @IBAction func closedButton(sender: AnyObject) {
         self.cookingSwitch.setOn(false, animated: true)
         switchState(cookingSwitch)
     }
     
+    // Open button for Switch
     @IBAction func openButton(sender: AnyObject) {
         self.cookingSwitch.setOn(true, animated: true)
         switchState(cookingSwitch)
     }
     
     
-    
-    
+    // Done button
     @IBAction func doneButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
