0a86eb5fbe0c0711ac83fcd202bdf25752b10c90
Young-hu Kim <youngk4u@yahoo.com>
2015-10-29 03:45:29 -0700

    Separated detail view from Fhooder view for the same reason.

---
+++ Fhood/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  Fhood
+//
+//  Created by YOUNG on 10/28/15.
+//  Copyright © 2015 YOUNG&YOUM. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: UIViewController {
+
+}
