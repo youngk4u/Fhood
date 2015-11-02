fc60536c70a4ca1f37d0374786f5d12e7d10021c
Young-hu Kim <youngk4u@yahoo.com>
2015-11-01 22:39:43 -0800

    Re-designed Ordered View to show the order list.

--- Fhood/OrderedViewController.swift
+++ Fhood/OrderedViewController.swift
@@ -10,8 +10,50 @@ import UIKit
 
 final class OrderedViewController: UIViewController {
 
+    @IBOutlet weak var PickupTimer: UINavigationBar!
+    @IBOutlet weak var orderedTime: UILabel!
+    @IBOutlet weak var orderStatus: UILabel!
+    @IBOutlet weak var fhooderName: UILabel!
     @IBOutlet private var address: UILabel!
-    @IBOutlet private var orderTime: UILabel!
+
+    
+    @IBOutlet weak var subtotal: UILabel!
+    var subtotalPassed: String = ""
+
+    @IBOutlet weak var taxesAndFees: UILabel!
+    var taxesAndFeesPassed: String = ""
+
+    @IBOutlet weak var total: UILabel!
+    var totalPassed: String = ""
+
+    
+    @IBOutlet weak var qtyOne: UILabel!
+    @IBOutlet weak var qtyTwo: UILabel!
+    @IBOutlet weak var qtyThree: UILabel!
+    @IBOutlet weak var qtyFour: UILabel!
+    @IBOutlet weak var qtyFive: UILabel!
+    @IBOutlet weak var qtySix: UILabel!
+    @IBOutlet weak var qtySeven: UILabel!
+    var qtyArray: [Int] = [0,0,0,0,0,0,0]
+    
+    @IBOutlet weak var itemOne: UILabel!
+    @IBOutlet weak var itemTwo: UILabel!
+    @IBOutlet weak var itemThree: UILabel!
+    @IBOutlet weak var itemFour: UILabel!
+    @IBOutlet weak var itemFive: UILabel!
+    @IBOutlet weak var itemSix: UILabel!
+    @IBOutlet weak var itemSeven: UILabel!
+    var itemArray: [String] = ["","","","","","",""]
+    
+    @IBOutlet weak var priceOne: UILabel!
+    @IBOutlet weak var priceTwo: UILabel!
+    @IBOutlet weak var priceThree: UILabel!
+    @IBOutlet weak var priceFour: UILabel!
+    @IBOutlet weak var priceFive: UILabel!
+    @IBOutlet weak var priceSix: UILabel!
+    @IBOutlet weak var priceSeven: UILabel!
+    var priceArray: [String] = ["","","","","","",""]
+    
     
     // Create Message Composer
     let messageComposer = MessageComposer()
@@ -21,8 +63,51 @@ final class OrderedViewController: UIViewController {
         
         self.tabBarController?.tabBar.hidden = true
         
+        //self.orderTime.text = "Ordered at \(fhoodie.orderedTime)"
+        self.fhooderName.text = variables.name!
         self.address.text = variables.address!
-        //self.orderTime.text = "\(variables.orderTimeHour):\(variables.orderTimeMinute) \(variables.orderTimeAmpm)"
+        
+        
+        //Passing values from previous View Controller
+        if self.qtyArray[0] != 0 {
+            self.qtyOne.text = String(self.qtyArray[0])
+            self.itemOne.text = self.itemArray[0]
+            self.priceOne.text = self.priceArray[0]
+        }
+        if self.qtyArray[1] != 0 {
+            self.qtyTwo.text = String(self.qtyArray[1])
+            self.itemTwo.text = self.itemArray[1]
+            self.priceTwo.text = self.priceArray[1]
+        }
+        if self.qtyArray[2] != 0 {
+            self.qtyThree.text = String(self.qtyArray[2])
+            self.itemThree.text = self.itemArray[2]
+            self.priceThree.text = self.priceArray[2]
+        }
+        if self.qtyArray[3] != 0 {
+            self.qtyFour.text = String(self.qtyArray[3])
+            self.itemFour.text = self.itemArray[3]
+            self.priceFour.text = self.priceArray[3]
+        }
+        if self.qtyArray[4] != 0 {
+            self.qtyFive.text = String(self.qtyArray[4])
+            self.itemFive.text = self.itemArray[4]
+            self.priceFive.text = self.priceArray[4]
+        }
+        if self.qtyArray[5] != 0 {
+            self.qtySix.text = String(self.qtyArray[5])
+            self.itemSix.text = self.itemArray[5]
+            self.priceSix.text = self.priceArray[5]
+        }
+        if self.qtyArray[6] != 0 {
+            self.qtySeven.text = String(self.qtyArray[6])
+            self.itemSeven.text = self.itemArray[6]
+            self.priceSeven.text = self.priceArray[6]
+        }
+        
+        self.subtotal.text = self.subtotalPassed
+        self.taxesAndFees.text = self.taxesAndFeesPassed
+        self.total.text = self.totalPassed
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -30,6 +115,7 @@ final class OrderedViewController: UIViewController {
         self.navigationController?.navigationBarHidden = true
         
     }
+    
     @IBAction func messageFhooder(sender: AnyObject) {
         
         // Make sure the device can send text messages
--- Fhood/ReceiptViewController.swift
+++ Fhood/ReceiptViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ReceiptViewController: UIViewController {
     
+    @IBOutlet weak var fhooderName: UILabel!
+    
     @IBOutlet weak var subTotal: UILabel!
     @IBOutlet weak var taxesAndFees: UILabel!
-    @IBOutlet weak var TotalAmountDue: UILabel!
+    @IBOutlet weak var totalAmountDue: UILabel!
     
     @IBOutlet weak var quantityOne: UILabel!
     @IBOutlet weak var quantityTwo: UILabel!
@@ -21,6 +23,7 @@ class ReceiptViewController: UIViewController {
     @IBOutlet weak var quantityFive: UILabel!
     @IBOutlet weak var quantitySix: UILabel!
     @IBOutlet weak var quantitySeven: UILabel!
+    var quantityArray: [Int] = [0,0,0,0,0,0,0]
     
     @IBOutlet weak var itemOne: UILabel!
     @IBOutlet weak var itemTwo: UILabel!
@@ -29,6 +32,7 @@ class ReceiptViewController: UIViewController {
     @IBOutlet weak var itemFive: UILabel!
     @IBOutlet weak var itemSix: UILabel!
     @IBOutlet weak var itemSeven: UILabel!
+    var itemNameArray: [String] = ["","","","","","",""]
     
     @IBOutlet weak var priceOne: UILabel!
     @IBOutlet weak var priceTwo: UILabel!
@@ -37,6 +41,7 @@ class ReceiptViewController: UIViewController {
     @IBOutlet weak var priceFive: UILabel!
     @IBOutlet weak var priceSix: UILabel!
     @IBOutlet weak var priceSeven: UILabel!
+    var priceTextArray: [String] = ["","","","","","",""]
 
     @IBOutlet weak var timePicker: UIDatePicker!
     
@@ -49,37 +54,60 @@ class ReceiptViewController: UIViewController {
         
         // Currency formatter
         self.formatter.numberStyle = .CurrencyStyle
+        
+        self.fhooderName.text = variables.name!
 
         // Put the order list on the receipt
         for var x = 0; x < fhoodie.selectedItemCount!.count; x++ {
             if x == 0 && x < fhoodie.selectedItemCount!.count {
                 self.quantityOne.text = String("\(fhoodie.selectedItemCount![0])")
+                self.quantityArray[0] = fhoodie.selectedItemCount![0]
                 self.itemOne.text = fhoodie.selectedItemNames![0]
+                self.itemNameArray[0] = fhoodie.selectedItemNames![0]
                 self.priceOne.text = formatter.stringFromNumber(fhoodie.selectedItemPrices![0] * Double(fhoodie.selectedItemCount![0]))
+                self.priceTextArray[0] = self.priceOne.text!
             } else if x == 1 && x < fhoodie.selectedItemCount!.count {
                 self.quantityTwo.text = String("\(fhoodie.selectedItemCount![1])")
+                self.quantityArray[1] = fhoodie.selectedItemCount![1]
                 self.itemTwo.text = fhoodie.selectedItemNames![1]
+                self.itemNameArray[1] = fhoodie.selectedItemNames![1]
                 self.priceTwo.text = formatter.stringFromNumber(fhoodie.selectedItemPrices![1] * Double(fhoodie.selectedItemCount![1]))
+                self.priceTextArray[1] = self.priceTwo.text!
             } else if x == 2 && x < fhoodie.selectedItemCount!.count {
                 self.quantityThree.text = String("\(fhoodie.selectedItemCount![2])")
+                self.quantityArray[2] = fhoodie.selectedItemCount![2]
                 self.itemThree.text = fhoodie.selectedItemNames![2]
+                self.itemNameArray[2] = fhoodie.selectedItemNames![2]
                 self.priceThree.text = formatter.stringFromNumber(fhoodie.selectedItemPrices![2] * Double(fhoodie.selectedItemCount![2]))
+                self.priceTextArray[2] = self.priceThree.text!
             } else if x == 3 && x < fhoodie.selectedItemCount!.count {
                 self.quantityFour.text = String("\(fhoodie.selectedItemCount![3])")
+                self.quantityArray[3] = fhoodie.selectedItemCount![3]
                 self.itemFour.text = fhoodie.selectedItemNames![3]
+                self.itemNameArray[3] = fhoodie.selectedItemNames![3]
                 self.priceFour.text = formatter.stringFromNumber(fhoodie.selectedItemPrices![3] * Double(fhoodie.selectedItemCount![3]))
+                self.priceTextArray[3] = self.priceFour.text!
             } else if x == 4 && x < fhoodie.selectedItemCount!.count {
                 self.quantityFive.text = String("\(fhoodie.selectedItemCount![4])")
+                self.quantityArray[4] = fhoodie.selectedItemCount![4]
                 self.itemFive.text = fhoodie.selectedItemNames![4]
+                self.itemNameArray[4] = fhoodie.selectedItemNames![4]
                 self.priceFive.text = formatter.stringFromNumber(fhoodie.selectedItemPrices![4] * Double(fhoodie.selectedItemCount![4]))
+                self.priceTextArray[4] = self.priceFive.text!
             } else if x == 5 && x < fhoodie.selectedItemCount!.count {
                 self.quantitySix.text = String("\(fhoodie.selectedItemCount![5])")
+                self.quantityArray[5] = fhoodie.selectedItemCount![5]
                 self.itemSix.text = fhoodie.selectedItemNames![5]
+                self.itemNameArray[5] = fhoodie.selectedItemNames![5]
                 self.priceSix.text = formatter.stringFromNumber(fhoodie.selectedItemPrices![5] * Double(fhoodie.selectedItemCount![5]))
+                self.priceTextArray[5] = self.priceSix.text!
             } else if x == 6 && x < fhoodie.selectedItemCount!.count {
                 self.quantitySeven.text = String("\(fhoodie.selectedItemCount![6])")
+                self.quantityArray[6] = fhoodie.selectedItemCount![6]
                 self.itemSeven.text = fhoodie.selectedItemNames![6]
+                self.itemNameArray[6] = fhoodie.selectedItemNames![6]
                 self.priceSeven.text = formatter.stringFromNumber(fhoodie.selectedItemPrices![6] * Double(fhoodie.selectedItemCount![6]))
+                self.priceTextArray[6] = self.priceSeven.text!
             } else {
                 return
             }
@@ -89,7 +117,7 @@ class ReceiptViewController: UIViewController {
         self.subTotal.text = formatter.stringFromNumber(fhoodie.selectedTotalItemPrice!)
         self.taxesAndFees.text = formatter.stringFromNumber(fhoodie.selectedTotalItemPrice! * 0.1 + 0.3)
         fhoodie.totalDue = fhoodie.selectedTotalItemPrice! + fhoodie.selectedTotalItemPrice! * 0.1 + 0.3
-        self.TotalAmountDue.text = formatter.stringFromNumber(fhoodie.totalDue!)
+        self.totalAmountDue.text = formatter.stringFromNumber(fhoodie.totalDue!)
 
 
     
@@ -103,7 +131,7 @@ class ReceiptViewController: UIViewController {
     }
     
     @IBAction func completeOrder(sender: AnyObject) {
-        let alert = UIAlertController(title: "Confirm your order", message:"The total amount of \(self.TotalAmountDue.text!) will be charged to your account. Would you like to proceed?", preferredStyle: .Alert)
+        let alert = UIAlertController(title: "Confirm your order", message:"The total amount of \(self.totalAmountDue.text!) will be charged to your account. Would you like to proceed?", preferredStyle: .Alert)
         let cancel = UIAlertAction(title: "Cancel", style: .Default) { _ in}
         let proceed = UIAlertAction(title: "Proceed", style: .Default) { (action: UIAlertAction!) -> () in
             self.performSegueWithIdentifier("toOrderedView", sender: self)
@@ -113,6 +141,20 @@ class ReceiptViewController: UIViewController {
         self.presentViewController(alert, animated: true){}
     }
     
+    //Passing order list values to the next View Controller
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "toOrderedView") {
+            let orderedVC = segue.destinationViewController as! OrderedViewController;
+            
+            orderedVC.qtyArray = self.quantityArray
+            orderedVC.itemArray = self.itemNameArray
+            orderedVC.priceArray = self.priceTextArray
+            
+            orderedVC.subtotalPassed = self.subTotal.text!
+            orderedVC.taxesAndFeesPassed = self.taxesAndFees.text!
+            orderedVC.totalPassed = self.totalAmountDue.text!
+        }
+    }
 
     // Close receipt
     @IBAction func closeReceiptView(sender: AnyObject) {
